88c5292d2db4416e83f28809640514d329787029
Jonas <iacobi.jonas@gmail.com>
2017-03-29 16:52:12 +0200

    Fix memory allocation, define maximum message length and filename, add explanatory comments to file

--- software/inputhandler.c
+++ software/inputhandler.c
@@ -1,13 +1,22 @@
-//This is a preliminary version of this file for testing and learning purposes
-//Author: Jonas Iacobi
+/*
+ *inputhandler.c will read a string from a text file, and format it so that the sign can use it.
+ *Finally it will send the formatted data to a program responsible for the wireless transmission.
+ *Author: Jonas Iacobi
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-char* getMessage(char* filename){
-  char* msg = NULL;
-  int   msgLength, txtLength;
+#define MAXMSGLENGTH 500
+#define FILENAME "C:/Users/iacob/Desktop/msg.txt"
+
+/*
+ * The getMessage function reads from the file specified and puts the content in msg.
+ * The specified file should contain nothing but text. Returns a reference to the changed string.
+*/
+char* getMessage(char* filename, char* msg){
+  int   msgLength;
   FILE *fp = fopen( filename, "r");
 
   if(fp){
@@ -16,32 +25,32 @@ char* getMessage(char* filename){
     msgLength = ftell(fp);
     fseek(fp, SEEK_SET, 0);
 
-    //Allocate memory for the string
-    msg = ((char*) malloc(sizeof(char) * msgLength+1));
-
     //Read the entire file pointed to by fp in chunks of 2 byte to the string msg and store the read size in txtLength.
-    txtLength = fread(msg, sizeof(char), msgLength, fp);
+    fread(msg, sizeof(char), msgLength, fp);
     msg[msgLength] = '\0';
     fclose(fp);
-    //Error check
-    if(msgLength != txtLength){
-      //The read failed somehow. Abort mission.
-      free(msg);
-      msg = NULL;
-      return "Error: Length of file not equal to length of read. That's probably my fault -Jonas";
-      }
 
     return msg;
     }
   else{
-    return "Error: File not found. Please contact the poor students who made this.";
+    return "Error: Message file failed to open. Please contact the poor students who made this.";
     }
 }
 
+/*
+ *Runs the functions getMessage, format, and transmit,
+ *as well as initializing and cleaning up the required variables.
+*/
 int main(){
-  char* filename = "C:/Users/iacob/Desktop/msg.txt";
-  printf("%s\n", filename);
-  char* message = getMessage(filename);
+  //Allocate memory for the message string, the same array will be overwritten for subsequent messages.
+  char* message = ((char*) malloc(sizeof(char) * MAXMSGLENGTH+1));
+
+  //Call getMessage to read the content of filename into the string message
+  getMessage(FILENAME, message);
   printf("%s\n", message);
+
+  //Free memory for the message string
+  free(message);
+  //Exit
   return 0;
 }
