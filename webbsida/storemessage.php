51dd8e8eae13d71a7f67fd32565da98f634ca3e9
benhed <benhed@kth.se>
2017-04-04 15:47:22 +0200

    website, index page, messagehandler done

---
+++ webbsida/storemessage.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['updateMsg'])) {
+  $file_data = $_POST['updateMsg'];
+  $file_data .= "|\r\n";
+  $file_data .= file_get_contents('msg/mydata.txt');
+  file_put_contents('msg/mydata.txt', $file_data);
+}
+else {
+   die('no post data to process');
+}
+header('Location: index.html');
+exit;
+?>
